4294ae6b7c7ff1765e8a18a3506f3074ccd78f6b
Anthony Gore <anthony@vuejsdevelopers.com>
2022-02-16 14:15:07 +1100

    Fixing meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The easiest way to create a full-featured, custom online course, exactly how you want it. Use your favorite frontend tools - no server required' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
